83b028b34ffebfc9859a0e10fd18cd889256b17b
deyvisgc <deivisgarcia@upeu.edu.pe>
2024-02-18 13:22:45 -0500

    fix config service

--- ms-products/src/main/java/com/example/product/SpringCloudApplication.java
+++ ms-products/src/main/java/com/example/product/SpringCloudApplication.java
@@ -2,9 +2,6 @@ package com.example.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan
 @SpringBootApplication
 public class SpringCloudApplication {
 
